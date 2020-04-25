61c3df86a68b25b9cb72da02dce330dd97630c58
Mallaiya M <30726733+Mallaiya@users.noreply.github.com>
2020-04-25 14:10:06 +0530

    Update dbconnections.php

--- assets/db/dbconnections.php
+++ assets/db/dbconnections.php
@@ -1,7 +1,7 @@
 <?php
     $dbserver = "127.0.0.1";
     $dbuser = "root";
-    $dbuserpassword = "Ksrce@123";
+    $dbuserpassword = "";
     $dbname = "user_database";
     
     $connection = mysqli_connect($dbserver, $dbuser, $dbuserpassword, $dbname);
